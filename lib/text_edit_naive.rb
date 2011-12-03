f70cedf2fedff91dad0457fd9e9131df7db0ffb4
Keith Zimmerman <keithrz@gmail.com>
2011-12-03 12:21:09 -0600

    re-implemented undo, commented out redo added results for unit tests and performance test

--- lib/text_edit_naive.rb
+++ lib/text_edit_naive.rb
@@ -13,32 +13,71 @@ module TextEditor
     # record the add and remove actions themselves
     
     # structure of action to record:
-    # { "add" => "text to add" }
-    # { "rm" => "text to remove" }
+    # ["add" , "text to add", -1]
+    # [ "rm" , "text to remove", 2]
     # with more time, this structure should be changed to an object
+    ## init 
+    # add Hello
+    # ["add", "Hello", -1]
+    # undo calls remove_text 0, "Hello".length
+    #
+    ## init 
+    # add Hello
+    # ["add", "Hello", -1]
+    # add World
+    # ["add", " World", -1]
+    # undo calls remove_text 5, "Hello".length
+    # 5 == the length of content before add (special case for position = -1)
+    #
+    ## init
+    # ["add", "Hal", -1]
+    # ["add", "skel", 2]
+    # undo calls remove_text 2, "skel".length
+    #
+    ## init
+    # call add_Text "Hello"
+    # call remove_text(1,3)
+    # ["rm", "el", 1] 
+    # undo calls add_text "el", 1
+    # 
     
-    def add_text(text, position=-1)
-      snapshot(true)
+    
+    def add_text(text, position=-1, snapshot_needed=true)
+      if snapshot_needed then
+        snapshot(true, "add", text, position == -1 ? @contents.length : position)
+      end
       contents.insert(position, text)
     end
 
-    def remove_text(first=0, last=contents.length)
-      snapshot(true)
-      contents.slice!(first...last)
+    def remove_text(first=0, last=contents.length, snapshot_needed=true)
+      # open question to product owner: 
+      #  any issues with removing the text
+      #  before recording the snapshot?
+      removed_text = contents.slice!(first...last)
+      if snapshot_needed then
+        snapshot(true, "rm", removed_text, first)
+      end
     end
 
     #change snapshot to only record the change made
-    def snapshot(clear_redo=false)
+    def snapshot(clear_redo=false, action="rm", text="", position=0)
       @reverted = [] if clear_redo
-      @snapshots << @contents.dup
+      @snapshots << [action, text, position]
     end
 
     #change reverted to only record t
     def undo
       return if @snapshots.empty?
 
-      @reverted << @contents
-      @contents = @snapshots.pop
+#      @reverted << @contents
+      # admittedly, the action array is confusing
+      # an object would be much more straightforward
+      action = @snapshots.pop
+      if action[0] == "add" then
+        remove_text action[2], action[2] + action[1].length, false
+      else
+        add_text action[1], action[2], false
+      end
     end
 
     def redo
